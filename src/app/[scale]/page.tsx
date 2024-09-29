71d3e80cd66524676cfc745d7773c3ba33d1ab5f
Dimosthenis Kalaitzis <dimostheniskalaitzis@gmail.com>
2024-09-30 01:18:52 +0300

    Add dominant chords

--- src/app/[scale]/page.tsx
+++ src/app/[scale]/page.tsx
@@ -1,29 +1,44 @@
 "use client";
 import { useParams } from "next/navigation";
 import PianoKeyboard from "../components/PianoKeyboard";
-import { note, theory } from "../lib";
+import { theory } from "../lib";
 
 export default function Index() {
     const { scale: scaleRoot  } = useParams();
-    const scale = theory.getScale(scaleRoot as note);
+    const scale = theory.getScale(scaleRoot as string);
+    const relativeScale = theory.getRelativeScale(scaleRoot as string);
+    const chords = theory.getScaleChords(scaleRoot as string);
+    const relativeChords = theory.getScaleChords(relativeScale.name);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-6xl font-bold">{scale.name} Scale Overview</h1>
-        <div className="flex justify-center">
+      <main className="flex flex-col gap-8 row-start-2 items-center w-full max-w-4xl">
+        <h1 className="text-6xl font-bold text-center">{scale.name} Scale Overview</h1>
+        <div className="w-full flex justify-center items-center">
           <PianoKeyboard scaleHighlightedNotes={scale.notes} pentatonicHighlightedNotes={scale.pentatonicNotes} />
         </div>
-        <div className="flex justify-center p-8">
-          <ol className="list-decimal">
-            {theory.getMajorScaleChords(scale.rootNote).map((chord) => (
-              <li key={chord.chordName}>
-              {chord.chordName}
-              </li>
-          ))}
-          </ol>
+        <div className="w-2/3 flex flex-col gap-8 justify-center items-center">
+          <div className="w-2/3">
+            <h2 className="text-4xl font-bold mb-4">Chords</h2>
+            <ol className="list-decimal pl-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2">
+              {chords.map((chord) => (
+                <li key={chord.chordName}>
+                  <span className={`${chord.isDominant ? 'text-red-500 font-semibold' : ''}`}>{chord.chordName}</span>
+                </li>
+              ))}
+            </ol>
+          </div>
+          <div className="w-2/3">
+            <h2 className="text-4xl font-bold mb-4">Relative Chords</h2>
+            <ol className="list-decimal pl-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2">
+              {relativeChords.map((chord) => (
+                <li key={chord.chordName}>
+                  <span className={`${chord.isDominant ? 'text-red-500 font-semibold' : ''}`}>{chord.chordName}</span>
+                </li>
+              ))}
+            </ol>
+          </div>
         </div>
-
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
--- src/app/lib/theory.ts
+++ src/app/lib/theory.ts
@@ -7,6 +7,8 @@ const majorScaleComposition: Array<chordType> = ['major', 'minor', 'minor', 'maj
 const minorScaleIntervals = [0, 2, 3, 5, 7, 8, 10];
 const pentatonicScaleIntervals = [1, 2, 3, 5, 6];
 
+const dominantScaleChordIntervals = [0, 3, 4, 5];
+
 const chordCompositions = {
     major: {
         intervals: [0, 4, 7],
@@ -40,6 +42,7 @@ const getMajorScale = (root: note) => {
     const pentatonicScaleNotes = scaleNotes.filter((note, index) => pentatonicScaleIntervals.includes(index));
 
     return {
+        type: 'major',
         name: `${root}`,
         rootNote: root,
         notes: scaleNotes,
@@ -53,11 +56,20 @@ const getMinorScale = (root: note) => {
     const pentatonicScaleNotes = scaleNotes.filter((note, index) => pentatonicScaleIntervals.includes(index));
 
     return {
+        type: 'minor',
         name: `${root}m`,
         rootNote: root,
         notes: scaleNotes,
         pentatonicNotes: pentatonicScaleNotes,
     }
+}
+
+const getRelativeScale = (scaleName: string) => {
+    const scaleNameSafe = scaleName.replace("sharp", "#").replace("flat", "b");
+    if (scaleNameSafe.endsWith("m")) {
+        return getRelativeMajorScale(scaleNameSafe.slice(0, -1) as note);
+    }
+    return getRelativeMinorScale(scaleNameSafe as note);
 }
 
 const getRelativeMinorScale = (majorScaleRoot: note) => {
@@ -87,16 +99,31 @@ const getChord = (root: note, type: chordType) => {
     }
 }
 
+const getScaleChords = (scaleName: string) => {
+    const scaleNameSafe = scaleName.replace("sharp", "#").replace("flat", "b");
+    let scaleChords = [];
+    if (scaleNameSafe.endsWith("m")) {
+        scaleChords = getMinorScaleChords(scaleNameSafe.slice(0, -1) as note);
+    }
+    scaleChords = getMajorScaleChords(scaleNameSafe as note);
+
+    return scaleChords.map((chord, index) => {
+        return {
+            ...chord,
+            isDominant: dominantScaleChordIntervals.includes(index),
+        }
+    });
+}
+
 const getMajorScaleChords = (majorScaleRootNote: note) => {
     const majorScale = getMajorScale(majorScaleRootNote);
-    const chords = majorScale.notes.map((note, index) => {
+    return majorScale.notes.map((note, index) => {
         const chordType = majorScaleComposition[index];
         if (!note) {
             throw new Error("Invalid root note");
         }
         return getChord(note, chordType);
     });
-    return chords;
 }
 
 const getMinorScaleChords = (minorScaleRootNote: note) => {
@@ -111,13 +138,6 @@ const getMinorScaleChords = (minorScaleRootNote: note) => {
 
 export const theory = {
     getScale,
-    getMajorScale,
-    getMinorScale,
-    getRelativeMinorScale,
-    getRelativeMajorScale,
-    getChord,
-    getMajorScaleChords,
-    getMinorScaleChords,
+    getScaleChords,
+    getRelativeScale,
 }
-
-console.log(theory.getMinorScale("A"))
